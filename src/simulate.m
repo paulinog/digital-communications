9924ef4aaa4e58e2f2384050f35e18e99cfdaf25
Paulino G <guilherme.paulino@msn.com>
2019-09-18 11:41:34 -0300

    Created mod_pam4 function

--- src/simulate.m
+++ src/simulate.m
@@ -9,15 +9,21 @@ VERBOSE = false;
 %% Modulation Proprierties (4-PAM)
 symbols_set = [-3, -1, 1, 3];
 
+
 %% Source
+% Text input by the user
 disp('Enter text:');
 array_bin = str_source();
+% Start a stopwatch timer
+tic;
 
 %% Mapper
 a = mapper(array_bin, symbols_set);
 
 %% Modulator
+% s = mod_pam4(a);
 %% Channel
+% r = s+n;
 z = a;
 %% Demodulator
 %% Demapper
@@ -25,6 +31,11 @@ out_array = demapper(z, symbols_set);
 
 %% Destination
 text_output = str_dest(out_array);
+% Stop stopwatch timer
+disp('-----')
+disp('Total time to compute:');
+toc;
+% Displays text output
 disp('-----')
 disp('Output text:');
 disp(text_output)
