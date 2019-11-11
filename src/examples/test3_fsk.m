bce53d963e660a9dad8e60bfb81df993fa8b3002
Paulino G <guilherme.paulino@msn.com>
2019-11-11 20:29:42 -0300

    Test3 FSK - FEC is working

--- src/examples/test3_fsk.m
+++ src/examples/test3_fsk.m
@@ -1,10 +1,10 @@
 clc; clearvars; 
 disp('FSK-2 example')
 %% User parameters
-numSymbol = 1000;
+numSymbol = 200;
 
 fc0 = 440;
-fc1 = 2*fc0;
+fc1 = 3*fc0;
 
 fs = 10*fc1;
 
@@ -13,9 +13,9 @@ trellis = poly2trellis(3, [5 7]);
 parity_ratio = 2;
 
 %% Frame Sync
-k = 10;
+k = 8;
 sync_bits = 2^k-1;
-max_peak_pos = sync_bits * 0.9;
+max_peak_pos = sync_bits * 0.8;
 
 % TEST channel delays
 % ch_delay1 = round(numSymbol*rand(1));
@@ -24,26 +24,32 @@ max_peak_pos = sync_bits * 0.9;
 %% Time vector
 timestep = 1/fs;
 T = 1/fc0;
+
+% MLS + FEC + Channel delays
 % tmax = (ch_delay1 + sync_bits + parity_ratio * numSymbol + ch_delay2) * T;
-tmax = (sync_bits + parity_ratio * numSymbol) * T;
+% MLS + FEC
+% tmax = (sync_bits + parity_ratio * numSymbol) * T;
+% FEC
+tmax = (parity_ratio * numSymbol) * T;
+
 t = 0:timestep:tmax-timestep;
 
 %% TX
 a = randsrc(1, numSymbol, [0 1]);
 a_enc = convenc(a, trellis);
-a_sync = [double(mls(k, 1) > 0) a_enc];
+% a_sync = [double(mls(k, 1) > 0) a_enc];
 
-h0 = sin(2*pi*fc0*t);
-h1 = sin(2*pi*fc1*t);
+h0 = sin(2*pi*fc0*t); % bit 0
+h1 = sin(2*pi*fc1*t); % bit 1
 
 w = heaviside(t) - heaviside(t - T);
 
-m = upsample(a_sync, T*fs);
+m = upsample(a_enc, T*fs);
 am = conv(m, w);
-% plot(am)
+% plot(am(1:length(t)))
 
-s = am(1:length(t)) .* h0 ...
-    + (1 - am(1:length(t))) .* h1;
+s = am(1:length(h0)) .* h1 ...
+    + (1 - am(1:length(h1))) .* h0;
 % plot(t, s)
 
 % sound(s, fs)
@@ -55,7 +61,7 @@ r = s;
 %% RX
 zci = @(v) find(v(:).*circshift(v(:), [-1 0]) <= 0);
 zx = zci(r);
-if (false)
+if (true)
     figure()
     plot(t, r, '-r')
     hold on
@@ -65,35 +71,44 @@ if (false)
     legend('Signal', 'Approximate Zero-Crossings')
 end
 
+%%
+
 len_r = length(r);
-
-% c = zeros(1, numSymbol * parity_ratio);
 c = zeros(1, len_r);
-c_aux = 1;
-% for i = 1 : (numSymbol * parity_ratio)
 for i = 1 : len_r
-    count = numel(find(t(zx) <= i*T));
+    count = numel(find(t(zx) > (i-1)*T & t(zx) <= i*T));
     c(i) = count;
-    c(i) = c(i) - c_aux;
-    c_aux = count;
 end
-c;
-y_enc = double(c <= 3);
+% % % c;
+% % % plot(t(1:numSymbol), c(1:numSymbol))
+% % % ylabel('c')
+y_enc = double(c(1:(parity_ratio * numSymbol)) >= 4);
+% % % y_sync = double(c(1:(parity_ratio * numSymbol + sync_bits + 1)) >= 4);
 
-self_corr = xcorr(y_enc, double(mls(k, 1) > 0));
-figure()
-plot(self_corr)
-
-start_frame = find(self_corr > max_peak_pos);
-% vec_rx_i = vec_rx(start_frame(i) + 1 : start_frame(i) + tam);
-
-% y = vitdec(y_enc, trellis, 20,  'trunc', 'hard');
+% %figure()
+% %plot(t(1:(parity_ratio * numSymbol)), y_sync)
 % 
-% %% BER
-% err = sum(a ~= y);
-% if (err >0 )
-%     error([num2str(err) ' errors'])
+% self_corr = xcorr(y_sync, double(mls(k, 1) > 0));
+% figure()
+% plot(self_corr(length(y_sync): end))
+% 
+% start_frame = find(self_corr(length(y_sync): end) > max_peak_pos);
+% 
+% if (start_frame(1) + parity_ratio*numSymbol) > length(y_sync)
+%     end_frame = length(y_sync);
 % else
-%     disp('no errors')
+%     end_frame = (start_frame(1) + parity_ratio*numSymbol);
 % end
+% 
+% y_enc = y_sync(start_frame(1) : end_frame);
+% %%
+y = vitdec(y_enc, trellis, 20,  'trunc', 'hard');
+
+%% BER
+err = sum(a ~= y);
+if (err >0 )
+    error([num2str(err) ' errors'])
+else
+    disp('no errors')
+end
 
