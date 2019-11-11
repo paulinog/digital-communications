5394605c8d2d5be1b693184cb145e7529bed075a
Paulino G <guilherme.paulino@msn.com>
2019-11-10 23:34:57 -0300

    Test 2 FSK - zero crossing counter

--- src/examples/test2_fsk.m
+++ src/examples/test2_fsk.m
@@ -1,12 +1,12 @@
 clc; clearvars; 
 disp('FSK-2 example')
 %% User parameters
-numSymbol = 1000;
+numSymbol = 10;
 
-fc0 = 440;
+fc0 = 880;
 fc1 = 2*fc0;
 
-fs = 100* fc0;
+fs = 20* fc1;
 
 %%
 timestep = 1/fs;
@@ -14,7 +14,7 @@ T = 1/fc0;
 tmax = numSymbol * T;
 t = 0:timestep:tmax-timestep;
 %% TX
-a = randsrc(1, numSymbol, [0 1]);
+a = randsrc(1, numSymbol, [0 1])
 
 h0 = sin(2*pi*fc0*t);
 h1 = sin(2*pi*fc1*t);
@@ -25,9 +25,42 @@ m = upsample(a, T*fs);
 am = conv(m, w);
 
 % plot(am)
-%% RX
+%% TX sent
 s = am(1:length(t)) .* h0 ...
     + (1 - am(1:length(t))) .* h1;
-plot(t, s)
 
-sound(s, fs)
+% figure();
+% plot(t, s)
+
+% sound(s, fs)
+
+%% Channel 
+% r = [zeros(1,100) mls s];
+r = s;
+
+%% RX
+zci = @(v) find(v(:).*circshift(v(:), [-1 0]) <= 0);
+
+zx = zci(r);
+
+if (false)
+    figure()
+    plot(t, r, '-r')
+    hold on
+    plot(t(zx), r(zx), 'bp')
+    hold off
+    grid
+    legend('Signal', 'Approximate Zero-Crossings')
+end
+
+c = zeros(1, numSymbol);
+c_aux = 1;
+for i = 1:numSymbol
+    count = numel(find(t(zx) <= i*T));
+    c(i) = count;
+    c(i) = c(i) - c_aux;
+    c_aux = count;
+end
+
+y = double(c <= 3)
+
