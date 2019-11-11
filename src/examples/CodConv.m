90510f783074699ffa223ea0879acef735363152
Paulino G <guilherme.paulino@msn.com>
2019-11-10 22:48:31 -0300

    cod conv - changes

--- src/examples/CodConv.m
+++ src/examples/CodConv.m
@@ -1,8 +1,21 @@
 clc;
-%Tx
+%% Poly to Trellis
+% Convert convolutional code polynomial to trellis description
+
 trellis = poly2trellis(3, [5 7]);
-tam=30;
-msg=randsrc(1,tam,[0 1]);
-msgENC = convenc(msg,trellis);
-msgDEC = vitdec(msgENC,trellis,20,'trunc','hard');
-sum(msg(2:end)~=msgDEC(2:end))
+
+%% Tx
+tam=1000;
+msg=randsrc(1, tam, [0 1]);
+
+msgENC = convenc(msg, trellis);
+
+%% Rx
+msgDEC = vitdec(msgENC, trellis, 20,  'trunc', 'hard');
+
+err = sum(msg(1:end)~=msgDEC(1:end));
+if (err > 0)
+    error([num2str(err) ' symbol errors'])
+else
+    disp('no errors')
+end
