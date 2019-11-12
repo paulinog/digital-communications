c8dba7ff2f5e8e898da23cb3f15c91e2d5555fb6
Paulino G <guilherme.paulino@msn.com>
2019-11-11 21:47:43 -0300

    Test3 FSK - frame size parameter

--- src/examples/test3_fsk.m
+++ src/examples/test3_fsk.m
@@ -1,7 +1,7 @@
 clc; clearvars; 
 disp('FSK-2 example')
 %% User parameters
-numSymbol = 1000;
+numSymbol = 200;
 
 fc0 = 440;
 fc1 = 4*fc0;
@@ -18,17 +18,19 @@ sync_bits = 2^k-1;
 max_peak_pos = sync_bits * 0.8;
 
 % TEST channel delays
-% ch_delay1 = round(numSymbol*rand(1));
-% ch_delay2 = round(numSymbol*rand(1));
+ch_delay1 = 100*round(numSymbol*rand(1)); % random spacing
+ch_delay2 = 100*round(numSymbol*rand(1));
 
 %% Time vector
 timestep = 1/fs;
 T = 1/fc0;
 
+frame_size = sync_bits + parity_ratio * numSymbol;
+
 % MLS + FEC + Channel delays
 % tmax = (ch_delay1 + sync_bits + parity_ratio * numSymbol + ch_delay2) * T;
 % MLS + FEC
-tmax = (sync_bits + parity_ratio * numSymbol) * T;
+tmax = frame_size * T;
 % FEC
 % tmax = (parity_ratio * numSymbol) * T;
 
@@ -52,18 +54,23 @@ am = conv(m, w);
 
 s = am(1:length(h0)) .* h1 ...
     + (1 - am(1:length(h1))) .* h0;
-% plot(t, s)
+plot(t, s)
 
 % sound(s, fs)
 
 %% Channel 
 % r = [zeros(1, ch_delay1) s zeros(1, ch_delay2)];
+% r = [zeros(1, ch_delay1) s];
 r = s;
+
+frame_size_rx = frame_size;
+tmax_rx = frame_size_rx * T;
+t_rx = 0:timestep:tmax-timestep;
 
 %% RX
 zci = @(v) find(v(:).*circshift(v(:), [-1 0]) <= 0);
 zx = zci(r);
-if (true)
+if (false)
     figure()
     plot(t, r, '-r')
     hold on
@@ -73,25 +80,25 @@ if (true)
     legend('Signal', 'Approximate Zero-Crossings')
 end
 
-%%
-
+%% RX zero crossing counter
 len_r = length(r);
 c = zeros(1, len_r);
 for i = 1 : len_r
     count = numel(find(t(zx) > (i-1)*T & t(zx) <= i*T));
     c(i) = count;
 end
-% c;
-% plot(t(1:numSymbol), c(1:numSymbol))
-% ylabel('c')
+plot(t(1:numSymbol), c(1:numSymbol))
+ylabel('c')
 
-% y_enc = double(c(1:(parity_ratio * numSymbol)) >= 3);
-y_sync = double(c(1:(parity_ratio * numSymbol + sync_bits)) >= 5);
+y_sync = double(c >= 5);
 % stem(y_sync)
-% 
-% self_corr = xcorr(y_sync, double(mls(k, 1) > 0));
-% figure()
-% plot(self_corr(length(y_sync): end))
+
+%% RX removing MLS
+sync_vec2 = double(mls(k, 1) > 0);
+
+self_corr = xcorr(y_sync, sync_vec2);
+figure()
+plot(self_corr(length(y_sync): length(y_sync) + frame_size));
 % 
 % start_frame = find(self_corr(length(y_sync): end) > max_peak_pos);
 % 
@@ -104,7 +111,14 @@ y_sync = double(c(1:(parity_ratio * numSymbol + sync_bits)) >= 5);
 % y_enc = y_sync(start_frame(1) : end_frame);
 
 %%
-y_enc = y_sync(256 : end);
+
+start_frame = 256;
+% start_frame = ch_delay1 + (sync_bits+1);
+end_frame = (start_frame-1) + (frame_size - sync_bits);
+
+y_enc = y_sync(start_frame : end_frame);
+
+%% FEC
 y = vitdec(y_enc, trellis, 20,  'trunc', 'hard');
 
 %% BER
